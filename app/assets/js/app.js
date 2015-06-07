522b704a0c231e24d9664665936d1b0f160f5d07
Adrian Kirsten <adrian.kirsten@traaidmark.com>
2015-06-07 14:21:26 +0200

    clean up submission

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -107,22 +107,6 @@ yakControllers.controller('yakListCtrl', ['$scope', '$http',
       
     //A.3. BUILD OBJECT ----------------------------------------------------------------------------------------------
 
-    $scope.form = {};
-    $scope.form.name = '';
-    $scope.form.email = '';
-    $scope.form.style = '';
-
-    console.log($scope.form);
-
-    $scope.user = {};
-        $scope.user.name = '';
-        $scope.user.email = '';
-    $scope.beer = {};
-        $scope.beer.style = '';
-        $scope.beer.styleOwn = '';
-        $scope.beer.logo = '';
-        $scope.beer.name ='';
-
     //A.3. END --------------------------------------------------------------------------------------------------------  
       
     //A.1. YAK HEAD LOGO DATA -----------------------------------------------------------------------------------------  
@@ -149,34 +133,13 @@ yakControllers.controller('yakListCtrl', ['$scope', '$http',
       
       $scope.bouForm = function() {
       
-        
-        var formObject = {
-        
-            "person" : {
-
-                "name" : $scope.user.name,
-                "email" : $scope.user.email
-
-            },
-            "theBeer" : {
-            
-                "style" : $scope.beer.style,
-                "logo" : $scope.beer.logo,
-                "name" : $scope.beer.name 
-        
-            }
-        
-        }
-        
-        console.log(formObject);
-      
         var endpoint = 'http://forgesimpledata.cloudapp.net/FatYakService.svc/InsertFatYakUser/'
         
         var theString = $scope.user.name + '/' + $scope.user.email + '/' + $scope.beer.style + '/' + $scope.beer.name + '/' + $scope.beer.logo
         
         $http.get( endpoint + theString).success(function(data) {
         
-          console.log (endpoint + theString);
+          console.log (data);
 
         });
           
